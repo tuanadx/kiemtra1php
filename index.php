275b9aae23aa34f39fd0b68b048122d58a176ce4
Anh Tuan <phananhtuan662004@gmail.com>
2025-04-23 11:40:38 +0700

    heheh

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+// Chuyển hướng tất cả yêu cầu đến public/index.php
+require_once __DIR__ . '/public/index.php'; 
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Starting PHP Server..."
+php -S 0.0.0.0:$PORT 
