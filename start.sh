13fd2caa8aace2bf9afef247a5f0f9a14b84a47a
Anh Tuan <phananhtuan662004@gmail.com>
2025-04-24 15:09:49 +0700

    jeje

--- start.sh
+++ start.sh
@@ -1,7 +1,2 @@
-#!/bin/bash
-
-# Đảm bảo biến môi trường PORT được đặt, sử dụng 8080 là giá trị mặc định
-PORT="${PORT:-8080}"
-
-# Khởi động máy chủ PHP với cổng từ biến môi trường
-php -S 0.0.0.0:$PORT 
+#!/bin/sh
+php -S 0.0.0.0:${PORT:-8080} 
