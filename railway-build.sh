b2f17d191df2f60536c6de51c7cfd26d2805bc7f
Anh Tuan <phananhtuan662004@gmail.com>
2025-04-24 14:55:25 +0700

    hehe

--- railway-build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Skipping build process..."
-echo "No Composer dependencies needed"
-
-# Make sure the script exits with success
-exit 0 
--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Sử dụng PORT từ Railway nếu có, mặc định là 8080
-SERVER_PORT=${PORT:-8080}
-echo "Starting PHP Server on port $SERVER_PORT..."
-php -S 0.0.0.0:$SERVER_PORT 
